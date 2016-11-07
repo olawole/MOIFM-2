8a0d46671593839b05e1184cf083e8d230215462
ucaboso <ucaboso@udxaw601.ad.ucl.ac.uk>
2016-11-07 13:18:38 +0000

    Margin

--- src/main/java/cs/ucl/moifm/util/Population.java
+++ src/main/java/cs/ucl/moifm/util/Population.java
@@ -14,7 +14,7 @@ public class Population {
 	
 	public static List<String> archive = new ArrayList<String>();
 	public static List<String> invalid = new ArrayList<String>();
-	public static Double[] MARGIN = new Double[]{30.0, 50.0, 0.01};
+	public static Double[] MARGIN = new Double[]{50.0, 100.0, 0.01};
 	
 	public Project project;
 	
